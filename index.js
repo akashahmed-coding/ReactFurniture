170650835c5d53f312600bf13adb9fe783ecc1e3
akashneha96@gmail.com <akashneha96@gmail.com>
2023-01-08 12:34:00 +0600

    first commit

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ const app = express()
 const path = require('path')
 app.use(express.json())
 const PORT = process.env.PORT || 5000
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static('client/build'))
-//     app.get("*",(req,res)=>{
-//         res.sendFile(path.join(__dirname,'client','build','index.html'))
-//     })
-// }
 
-app.use(express.static(path.join(__dirname, './client/build')))
-app.get("*",(req,res)=>{
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get("*",(req,res)=>{
         res.sendFile(path.join(__dirname,'client','build','index.html'))
-})
+    })
+}
+
+// app.use(express.static(path.join(__dirname, './client/build')))
+// app.get("*",(req,res)=>{
+//         res.sendFile(path.join(__dirname,'client','build','index.html'))
+// })
 app.listen(PORT,()=>{
     console.log(`server runnig successfull at ${PORT}`)
 }) 
